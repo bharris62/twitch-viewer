757bd50b0837ae5360e85b8858bbae132d2a10af
Andrew Harris <BHarris@Andrews-MacBook-Pro.local>
2016-05-15 19:32:40 -0500

    added a loop and appeneded the stream names to the page.

--- App/script.js
+++ App/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-  var twitchApi = 'https://api.twitch.tv/kraken/streams/wintergaming?callback=?';
 
   var streamers = ['freecodecamp', 'storbeck', 'terakilobyte', 'syndicate', 'riotgames',
-        'summit1g', 'RobotCaleb', 'wintergaming'];
+  'summit1g', 'RobotCaleb', 'wintergaming'];
+  var name = '';
+  var twitchApi = 'https://api.twitch.tv/kraken/streams/' + name + '?callback=?';
 
   $.ajax({
     type: 'GET',
@@ -11,6 +12,18 @@ $(document).ready(function(){
 
     success: function(data){
       console.log(data);
+
+      for (i=0; i < streamers.length; i++){
+        name = streamers[i];
+        $('#link-output').append("<h1>" + name + "</h1><br>");
+
+      }
+
+
+
+
+
+
     }
   });
 
