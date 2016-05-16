9c02192bf4cd773581e7a7117685e1d6d9b3e154
Andrew Harris <BHarris@Andrews-MacBook-Pro.local>
2016-05-15 22:17:19 -0500

    added function to note if offline or account closed.

--- App/script.js
+++ App/script.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
   var name = '';
   var twitchApi = 'https://api.twitch.tv/kraken/streams/' + name + '?callback=?';
 
+function returnGame(d){
+  if (d === null){
+    return "Streamer is offline";
+  }else if(d === undefined){
+    return "Channel has been closed";
+  }else{
+    return(data.stream.game);
+  }
+}
+
+
   $.ajax({
     type: 'GET',
     url: twitchApi,
@@ -15,7 +26,8 @@ $(document).ready(function(){
 
       for (i=0; i < streamers.length; i++){
         name = streamers[i];
-        $('#link-output').append("<h1>" + name + "</h1><br>");
+        $('#link-output').append("<h1>" + name + "</h1> <h3>" returnGame(data) + "</h3><br>");
+        console.log(data);
 
       }
 
