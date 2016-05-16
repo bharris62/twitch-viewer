fbb790dc3d9dd4fd1f3a13e69ce97a2264d2cb06
Andrew Harris <BHarris@Andrews-MacBook-Pro.local>
2016-05-15 19:10:22 -0500

    added a dummy twitch call, and tested to make sure it would call to console.

--- App/script.js
+++ App/script.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-  $('#test').html("From Javascript, are ya workin?");
+  var twitchApi = 'https://api.twitch.tv/kraken/streams/wintergaming?callback=?';
+
+  var streamers = ['freecodecamp', 'storbeck', 'terakilobyte', 'syndicate', 'riotgames',
+        'summit1g', 'RobotCaleb', 'wintergaming'];
+
+  $.ajax({
+    type: 'GET',
+    url: twitchApi,
+    dataType: 'json',
+
+    success: function(data){
+      console.log(data);
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
