22cd54b74ab25ebe15c0553875f77e2103d5c69d
Andrew Harris <BHarris@Andrews-MacBook-Pro.local>
2016-05-16 21:17:42 -0500

    refactored

--- App/script.js
+++ App/script.js
@@ -1,62 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   var streamers = ['freecodecamp', 'storbeck', 'terakilobyte', 'syndicate', 'riotgames',
-  'summit1g', 'RobotCaleb', 'wintergaming', 'ESL_SC2'];
-  var name = '';
-  var twitchApi = 'https://api.twitch.tv/kraken/streams/' + name + '?callback=?';
-  //var twitchApi = 'https://api.twitch.tv/kraken/streams/freecodecamp?callback=?';
+    'summit1g', 'RobotCaleb', 'wintergaming', 'ESL_SC2'
+  ];
 
+  for (i = 0; i < streamers.length; i++) {
+    var name = streamers[i];
 
+    var twitchApi = 'https://api.twitch.tv/kraken/streams/' + name + '?callback=?';
 
-  $.ajax({
-    type: 'GET',
-    url: twitchApi,
-    dataType: 'json',
+   // var twitchApi = 'https://api.twitch.tv/kraken/streams/freecodecamp?callback=?';
 
+    $.ajax({
+      type: 'GET',
+      url: twitchApi,
+      dataType: 'json',
 
-    success: function(data){
-      console.log(data.stream);
+      success: function(data) {
+        //console.log(data);
 
-      function returnGame(d){
-        if (d === null){
-          return "Streamer is offline";
-        }else if(d === undefined){
-          return "Channel has been closed";
-        }else{
-          return(data.stream.game);
+        function returnGame(d) {
+          if (d === null) {
+            return "Streamer is offline";
+          } else if (d !== null) {
+            return d.game;
+          } else {
+            return "Channel has been closed";
+          }
         }
-      }
 
+        //console.log(data._links.self.substring(data._links.self.lastIndexOf('/')+1));
+        var channelName = data._links.self.substring(data._links.self.lastIndexOf('/')+1);
 
-      for (i=0; i < streamers.length; i++){
-        var name = streamers[i];
-        var descrip = returnGame(data.stream);
-        console.log(descrip);
+        var gameDescrip = returnGame(data.stream);
 
-        $('#link-output').append("<h1>" + name + "</h1> <h3>" + descrip + "</h3><br>");
-        console.log(data);
+        $('#link-output').append("<h1>" + channelName + "</h1> <h3>" + gameDescrip + "</h3><br>");
 
       }
-
-
-
-
-
-
-    }
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
+  }
 
 });
