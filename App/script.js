48d0d4cd7b2979a9a40b681da33eca738d75d850
Andrew Harris <BHarris@Andrews-MacBook-Pro.local>
2016-05-16 19:44:16 -0500

    need to do some debugging, having issues with returnGame func

--- App/script.js
+++ App/script.js
@@ -1,20 +1,11 @@
 $(document).ready(function(){
 
   var streamers = ['freecodecamp', 'storbeck', 'terakilobyte', 'syndicate', 'riotgames',
-  'summit1g', 'RobotCaleb', 'wintergaming'];
+  'summit1g', 'RobotCaleb', 'wintergaming', 'ESL_SC2'];
   var name = '';
-  //var twitchApi = 'https://api.twitch.tv/kraken/streams/' + name + '?callback=?';
-  var twitchApi = 'https://api.twitch.tv/kraken/streams/esl_sc2?callback=?';
+  var twitchApi = 'https://api.twitch.tv/kraken/streams/' + name + '?callback=?';
+  //var twitchApi = 'https://api.twitch.tv/kraken/streams/freecodecamp?callback=?';
 
-function returnGame(d){
-  if (d === null){
-    return "Streamer is offline";
-  }else if(d === undefined){
-    return "Channel has been closed";
-  }else{
-    return(data.stream.game);
-  }
-}
 
 
   $.ajax({
@@ -22,16 +13,28 @@ function returnGame(d){
     url: twitchApi,
     dataType: 'json',
 
+
     success: function(data){
-      console.log(data);
-      console.log(data.stream.game);  //accesses the stream game.
-      console.log();
+      console.log(data.stream);
+
+      function returnGame(d){
+        if (d === null){
+          return "Streamer is offline";
+        }else if(d === undefined){
+          return "Channel has been closed";
+        }else{
+          return(data.stream.game);
+        }
+      }
 
 
       for (i=0; i < streamers.length; i++){
-        //name = streamers[i];
-        //$('#link-output').append("<h1>" + name + "</h1> <h3>" returnGame(data) + "</h3><br>");
-        //console.log(data);
+        var name = streamers[i];
+        var descrip = returnGame(data.stream);
+        console.log(descrip);
+
+        $('#link-output').append("<h1>" + name + "</h1> <h3>" + descrip + "</h3><br>");
+        console.log(data);
 
       }
 
